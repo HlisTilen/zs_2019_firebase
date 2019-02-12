49ba4fddd4e29438b9f2076738edaa5d0a8bad67
Matej Drobnič <matejdro@gmail.com>
2019-02-12 15:17:48 +0100

    Add firebase

--- app/src/main/java/com/zimskasola/myawesometodoapp/LoginActivity.kt
+++ app/src/main/java/com/zimskasola/myawesometodoapp/LoginActivity.kt
@@ -2,21 +2,56 @@ package com.zimskasola.myawesometodoapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private val firebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         signInButton.setOnClickListener {
-            openMainScreen()
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email.isBlank() || password.isBlank()) {
+                return@setOnClickListener
+            }
+
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {result ->
+                    if (result.isSuccessful) {
+                        openMainScreen()
+                    } else {
+                        Toast.makeText(this@LoginActivity, "Napaka pri prijavi: ${result.exception}", Toast.LENGTH_LONG).show()
+                    }
+                }
         }
 
         registerButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
 
+            if (email.isBlank() || password.isBlank()) {
+                return@setOnClickListener
+            }
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { result ->
+                    if (result.isSuccessful) {
+                        openMainScreen()
+                    } else {
+                        Toast.makeText(this@LoginActivity, "Napaka pri registraciji: ${result.exception}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        if (firebaseAuth.currentUser != null) {
+            openMainScreen()
         }
     }
 
--- app/src/main/java/com/zimskasola/myawesometodoapp/MainActivity.kt
+++ app/src/main/java/com/zimskasola/myawesometodoapp/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val todoItems = ArrayList<String>()
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseFirestore = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +24,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         refreshButton.setOnClickListener {
-            //TODO
         }
+
+        logoutButton.setOnClickListener {
+            firebaseAuth.signOut()
+            finish()
+        }
+
+        updateList()
     }
 
     private fun showNewEntryDialog() {
@@ -38,13 +48,55 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addNewEntry(entry: String) {
-        todoItems.add(entry)
-        updateList()
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser == null) {
+            return
+        }
+
+        val userId = currentUser.uid
+        val collectionPath = "/users/$userId/todos"
+
+        firebaseFirestore.collection(collectionPath).add(TodoItem(entry))
+            .addOnCompleteListener { result ->
+                if (result.isSuccessful) {
+                } else {
+                    Toast.makeText(this@MainActivity, "Napaka: ${result.exception}", Toast.LENGTH_LONG).show()
+                }
+            }
+
     }
 
     private fun updateList() {
-        // Ne uporabljat ListView tako kot tukaj, da se vsakič nastavi nov adapter
-        // Tukaj je narejeno tako samo zato, da je koda manj zapletena
-        itemsListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todoItems)
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser == null) {
+            return
+        }
+
+        val userId = currentUser.uid
+        val collectionPath = "/users/$userId/todos"
+
+        firebaseFirestore.collection(collectionPath)
+            .addSnapshotListener { data, error ->
+                if (data != null) {
+                    // Naloži podatke
+
+                    val todoItems = data.documents.map { it.toObject(TodoItem::class.java)?.text }
+                    itemsListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todoItems)
+                } else {
+                    Toast.makeText(this, "Napaka $error", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+}
+
+class TodoItem {
+    val text: String
+
+    constructor(text: String) {
+        this.text = text
+    }
+
+    constructor() {
+        this.text = ""
     }
 }
